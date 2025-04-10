7afc69c2786a1ff7cdb796853532f2c8a0829279
Tameka Roberson <TTRoberson@student.fullsail.edu>
2025-04-10 15:31:19 -0400

    Task Completed

--- js/script.js
+++ js/script.js
@@ -1,30 +1,61 @@
 //Create 5 functions !PRACTICE FAT ARROW FUNCTION FORMAT!
 
-// Dark Mode (Accessibility/Readibility) 
-toggleDarkMode = () =>{
-    document.body.classList.toggle("dark-mode")
+// random Project Picker (Homepage (Later Date))
+// Create multiple project ideas in an array in global scope
+// Initialize a str var that take the length of a quote(s)
+// Use both math.floor and math.random to generate numbers within a specified range: An array of projects
+// print out a random project when function is called 
+
+let projects = [
+    "Portfolio", "Blog"
+];
+
+const projectRandomizer = () =>{
+    let projectLength = projects.length;
+    let ranIndex = Math.floor(Math.random() * projectLengthLength);
+    return projects[ranIndex];
 }
+
+// Function Tester
+console.log(projectRandomizer());
 
 // Contact Storage (Nested Arrays)
 // Initialize an array in global scope
-// put args (both names, email, messages) into an object
-// push object into the array
-// return array
-let storage = [];
+// put args (both names, email, messages) into another array
+// push filled array into the empty array
+// return nested array
+let contactData = [];
 
 const contactStorage = (firstName, lastName, email, message) =>{
     let userData = [];
     userData.push(firstName, lastName, email, message)
-    storage.push(userData);
-    return storage;
+    contactData.push(userData);
+    return contactData;
 }
 
 // Function Tester
 console.log(contactStorage("Jane", "Doe", "jDoe1@gmail.com", "I need help"));
 
+// feedback Storage
+// Initialize an array in global scope
+// put args (last name and messages) into an object
+// push object into the array
+// return array
+
+let feedbacks = [];
+
+const feedbackStorage = (lastName, message) =>{
+    feebacks.push({lastName : message});
+    return feedbacks;
+}
+
+// Function Tester
+console.log(feedbackStorage("Doe", "Great App!"));
+
 // Display Inspirational Quote Randomizer (Homepage (Later Date))
-// import Random Package
-// Create multiple quotes in an array
+// Create multiple quotes in an array in global scope
+// Initialize a str var that take the length of a quote(s)
+// Use both math.floor and math.random to generate numbers within a specified range: An array of quotes
 // print out a random quote when function is called 
 
 let quotes = [
@@ -34,14 +65,25 @@ let quotes = [
 ];
 
 const quoteRandomizer = () =>{
-    let ranIndex = Math.floor(Math.random() * quotes.length);
+    let quoteLength = quotes.length;
+    let ranIndex = Math.floor(Math.random() * quoteLength);
     return quotes[ranIndex];
 }
 
 // Function Tester
 console.log(quoteRandomizer());
 
+// Email Validation 
+// Check to see is email contains "@" and ".", if true:
+    // Return email
+//else:
+    // Return an error message
 
-
-
+    const emailValidation = (email) =>{
+        return email.includes("@") && email.includes(".") ? email : "email is invalid";
+    }
+    
+    // Function Tester
+    console.log(emailValidation("tyki193@gmail.com"));
+    console.log(emailValidation("tyki193gmail.com"));
 
